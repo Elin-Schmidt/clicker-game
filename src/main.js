b5c77ef9c143e552e5cdf6e629d4785528e3d412
Elin <elin.schmidt@gmail.com>
2025-01-29 17:00:55 +0100

    changed project name

--- src/main.js
+++ src/main.js
@@ -3,4 +3,11 @@ import './style.css';
 import App from './App.vue';
 import router from './router';
 
-createApp(App).use(router).mount('#app');
+// Bootstrap CSS
+import 'bootstrap/dist/css/bootstrap.css';
+// BootstrapVue 3 styles (om du använder BootstrapVue-specifika komponenter)
+import 'bootstrap-vue-3/dist/bootstrap-vue-3.css';
+
+import BootstrapVue3 from 'bootstrap-vue-3';
+
+createApp(App).use(router).use(BootstrapVue3).mount('#app');
