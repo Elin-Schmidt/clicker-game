b3601060136b1a4ef6a43e9935494eef4ebac710
Elin <elin.schmidt@gmail.com>
2025-02-28 22:56:42 +0100

    Adjust logo width and margin-top in App.vue for improved layout consistency and add box shadow to navbar

--- src/App.vue
+++ src/App.vue
@@ -80,14 +80,16 @@
     background-color: whitesmoke;
   }
   .logo {
-    width: 90px;
+    width: 80px;
     transition: all 0.3s ease-in-out;
   }
   .custom-navbar {
     background-color: #eee3e3;
     color: #2f4f4f;
     list-style: none;
-    margin-top: 120px;
+    margin-top: 110px;
+    box-shadow: rgba(0, 0, 0, 0.1) 0px 10px 15px -3px,
+      rgba(0, 0, 0, 0.05) 0px 4px 6px -2px;
   }
 
   @media (max-width: 991px) {
